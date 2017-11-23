41b958b194890fa92df980b124bc08091e42431b
Chris Qiu <orange.lavender.9@gmail.com>
2017-11-23 17:54:46 +1030

    Algorithms Part I - Week 1 Percolation: add test case

--- src/test/java/part1/week1/PercolationTest.java
+++ src/test/java/part1/week1/PercolationTest.java
@@ -1,9 +1,33 @@
 package part1.week1;
 
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.StdDraw;
 import org.junit.Test;
 
+import java.net.URL;
+
+import static part1.week1.PercolationVisualizer.draw;
+
 public class PercolationTest {
+    // delay in miliseconds (controls animation speed)
+    private static final int DELAY = 100;
+
     @Test
-    public void a() {
+    public void visualise() {
+        URL filePath = Percolation.class.getResource("/part1/week1/percolation/input1.txt");
+        In in = new In(filePath);
+        int n = in.readInt();                   // n-by-n percolation system
+        StdDraw.enableDoubleBuffering();        // turn on animation mode
+        Percolation perc = new Percolation(n);  // repeatedly read in sites to open and draw resulting system
+        draw(perc, n);
+        StdDraw.show();
+        StdDraw.pause(DELAY);
+        while (!in.isEmpty()) {
+            int i = in.readInt(), j = in.readInt();
+            perc.open(i, j);
+            draw(perc, n);
+            StdDraw.show();
+            StdDraw.pause(DELAY);
+        }
     }
 }
