a94824fdc7fa42dbc5df5599b2e37dca937fda1a
Chris Qiu <orange.lavender.9@gmail.com>
2017-11-28 12:44:11 +1030

    Part II - Week 2 Seam Carving

---
+++ src/main/java/SeamCarver.java
@@ -0,0 +1,57 @@
+import edu.princeton.cs.algs4.Picture;
+
+public class SeamCarver {
+    // create a seam carver object based on the given picture
+    public SeamCarver(Picture picture) {
+        // TODO Not Implemented
+        throw new UnsupportedOperationException();
+    }
+
+    // current picture
+    public Picture picture() {
+        // TODO Not Implemented
+        throw new UnsupportedOperationException();
+    }
+
+    // width of current picture
+    public int width() {
+        // TODO Not Implemented
+        throw new UnsupportedOperationException();
+    }
+
+    // height of current picture
+    public int height() {
+        // TODO Not Implemented
+        throw new UnsupportedOperationException();
+    }
+
+    // energy of pixel at column x and row y
+    public double energy(int x, int y) {
+        // TODO Not Implemented
+        throw new UnsupportedOperationException();
+    }
+
+    // sequence of indices for horizontal seam
+    public int[] findHorizontalSeam() {
+        // TODO Not Implemented
+        throw new UnsupportedOperationException();
+    }
+
+    // sequence of indices for vertical seam
+    public int[] findVerticalSeam() {
+        // TODO Not Implemented
+        throw new UnsupportedOperationException();
+    }
+
+    // remove horizontal seam from current picture
+    public void removeHorizontalSeam(int[] seam) {
+        // TODO Not Implemented
+        throw new UnsupportedOperationException();
+    }
+
+    // remove vertical seam from current picture
+    public void removeVerticalSeam(int[] seam) {
+        // TODO Not Implemented
+        throw new UnsupportedOperationException();
+    }
+}
