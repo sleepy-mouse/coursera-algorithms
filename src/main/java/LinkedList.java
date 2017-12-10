04d035f91dd08c4b520cde300607b6d424a106d4
Chris Qiu <orange.lavender.9@gmail.com>
2017-12-11 01:23:00 +1030

    Part I - Week 2 Deques and Randomized Queues: add iterator implementation

--- src/main/java/LinkedList.java
+++ src/main/java/LinkedList.java
@@ -1,4 +1,5 @@
 import java.util.Collection;
+import java.util.Deque;
 import java.util.Iterator;
 import java.util.List;
 import java.util.ListIterator;
@@ -6,15 +7,29 @@ import java.util.ListIterator;
 /**
  * @author Chris Qiu
  */
-class LinkedList<E> implements List<E> {
+class LinkedList<E> implements List<E>, Deque<E> {
+    private Node<E> first;
+    private Node<E> last;
+    private int size;
+
+    private class Node<E> {
+        Node<E> prev;
+        Node<E> next;
+        E object;
+
+        Node(E element) {
+            object = element;
+        }
+    }
+
     @Override
     public int size() {
-        return 0;
+        return size;
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return size == 0;
     }
 
     @Override
@@ -24,12 +39,17 @@ class LinkedList<E> implements List<E> {
 
     @Override
     public Iterator<E> iterator() {
-        return null;
+        return new LinkedListIterator<>(first);
     }
 
     @Override
     public Object[] toArray() {
-        return new Object[0];
+        Object[] objects = new Object[size];
+        int i = 0;
+        for (E e : this) {
+            objects[i++] = e;
+        }
+        return objects;
     }
 
     @Override
@@ -39,7 +59,8 @@ class LinkedList<E> implements List<E> {
 
     @Override
     public boolean add(E e) {
-        return false;
+        addLast(e);
+        return true;
     }
 
     @Override
@@ -74,7 +95,6 @@ class LinkedList<E> implements List<E> {
 
     @Override
     public void clear() {
-
     }
 
     @Override
@@ -89,7 +109,18 @@ class LinkedList<E> implements List<E> {
 
     @Override
     public void add(int index, E element) {
-
+        if (index < 0) {
+            if (first == null) {
+                first = new Node<>(element);
+            } else {
+                Node<E> n = first;
+                while (n.next != null) {
+                    n = n.next;
+                }
+                n.next = new Node<>(element);
+            }
+        } else {
+        }
     }
 
     @Override
@@ -109,16 +140,231 @@ class LinkedList<E> implements List<E> {
 
     @Override
     public ListIterator<E> listIterator() {
-        return null;
+        return new LinkedListIterator<>(first);
     }
 
     @Override
     public ListIterator<E> listIterator(int index) {
+        Node<E> e = find(index);
+        return new LinkedListIterator<>(e);
+    }
+
+    private Node<E> find(int index) {
         return null;
     }
 
     @Override
     public List<E> subList(int fromIndex, int toIndex) {
         return null;
+    }
+
+    @Override
+    public void addFirst(E e) {
+        add(0, e);
+    }
+
+    @Override
+    public void addLast(E e) {
+        if (size == 0)
+            add(0, e);
+        else
+            add(size - 1, e);
+    }
+
+    @Override
+    public boolean offerFirst(E e) {
+        return false;
+    }
+
+    @Override
+    public boolean offerLast(E e) {
+        return false;
+    }
+
+    @Override
+    public E removeFirst() {
+        return null;
+    }
+
+    @Override
+    public E removeLast() {
+        return null;
+    }
+
+    @Override
+    public E pollFirst() {
+        return null;
+    }
+
+    @Override
+    public E pollLast() {
+        return null;
+    }
+
+    @Override
+    public E getFirst() {
+        return null;
+    }
+
+    @Override
+    public E getLast() {
+        return null;
+    }
+
+    @Override
+    public E peekFirst() {
+        return null;
+    }
+
+    @Override
+    public E peekLast() {
+        return null;
+    }
+
+    @Override
+    public boolean removeFirstOccurrence(Object o) {
+        return false;
+    }
+
+    @Override
+    public boolean removeLastOccurrence(Object o) {
+        return false;
+    }
+
+    @Override
+    public boolean offer(E e) {
+        return false;
+    }
+
+    @Override
+    public E remove() {
+        return null;
+    }
+
+    @Override
+    public E poll() {
+        return null;
+    }
+
+    @Override
+    public E element() {
+        return null;
+    }
+
+    @Override
+    public E peek() {
+        return null;
+    }
+
+    @Override
+    public void push(E e) {
+    }
+
+    @Override
+    public E pop() {
+        return null;
+    }
+
+    @Override
+    public Iterator<E> descendingIterator() {
+        return new DescendingListIterator<>(last);
+    }
+
+    private class LinkedListIterator<T> implements ListIterator<T> {
+        Node<T> current;
+
+        LinkedListIterator(Node<T> start) {
+            this.current = start;
+        }
+
+        @Override
+        public boolean hasNext() {
+            return current.next != null;
+        }
+
+        @Override
+        public T next() {
+            return current.next.object;
+        }
+
+        @Override
+        public boolean hasPrevious() {
+            return current.prev != null;
+        }
+
+        @Override
+        public T previous() {
+            return current.prev.object;
+        }
+
+        @Override
+        public int nextIndex() {
+            return 0;
+        }
+
+        @Override
+        public int previousIndex() {
+            return 0;
+        }
+
+        @Override
+        public void remove() {
+        }
+
+        @Override
+        public void set(T e) {
+        }
+
+        @Override
+        public void add(T e) {
+        }
+    }
+
+    private class DescendingListIterator<R> extends LinkedListIterator<R> {
+        DescendingListIterator(Node<R> start) {
+            super(start);
+        }
+
+        @Override
+        public boolean hasNext() {
+            return current.prev != null;
+        }
+
+        @Override
+        public R next() {
+            return current.prev.object;
+        }
+
+        @Override
+        public boolean hasPrevious() {
+            return current.next != null;
+        }
+
+        @Override
+        public R previous() {
+            return current.next.object;
+        }
+
+        @Override
+        public int nextIndex() {
+            return 0;
+        }
+
+        @Override
+        public int previousIndex() {
+            return 0;
+        }
+
+        @Override
+        public void remove() {
+        }
+
+        @Override
+        public void set(R r) {
+        }
+
+        @Override
+        public void add(R r) {
+        }
     }
 }
