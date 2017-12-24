8a44a95ae1ea7162193d4dccd5deab12e2a2e40e
Chris Qiu <orange.lavender.9@gmail.com>
2017-12-25 01:28:09 +1030

    Part I - Week 3 Pattern Recognition: remove add brute-force implementation

--- src/main/java/FastCollinearPoints.java
+++ src/main/java/FastCollinearPoints.java
@@ -1,27 +1,34 @@
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author Chris Qiu
  */
 public class FastCollinearPoints {
-    private final Point[] points;
-    private LineSegment[] segments;
+    private final List<LineSegment> segments;
 
     // finds all line segments containing 4 or more points
     public FastCollinearPoints(Point[] points) {
         Util.validate(points);
-        this.points = points;
-        segments = new LineSegment[points.length];
-        for (int i = 0; i < points.length - 1; i++) {
-            segments[i] = new LineSegment(points[i], points[i + 1]);
+        double s1, s2, s3;
+        segments = new ArrayList<>(points.length);
+        for (int i = 0; i + 3 < points.length; i = i + 4) {
+            s1 = points[i].slopeTo(points[i + 1]);
+            s2 = points[i].slopeTo(points[i + 2]);
+            s3 = points[i].slopeTo(points[i + 3]);
+            if (s1 == s2 && s1 == s3 && s2 == s3) {
+                segments.add(new LineSegment(points[i], points[i + 3]));
+            }
         }
     }
 
     // the number of line segments
     public int numberOfSegments() {
-        return segments.length;
+        return segments.size();
     }
 
     // the line segments
     public LineSegment[] segments() {
-        return segments;
+        return segments.toArray(new LineSegment[segments.size()]);
     }
 }
