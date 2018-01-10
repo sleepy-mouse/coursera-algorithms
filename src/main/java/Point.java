a405c6f7478ee432e6151f65251c66bcc514e56a
Chris Qiu <orange.lavender.9@gmail.com>
2018-01-10 21:48:54 +1030

    Part I - Week 3 Pattern Recognition: add test code for drawing

--- src/main/java/Point.java
+++ src/main/java/Point.java
@@ -8,8 +8,11 @@
  *
  ******************************************************************************/
 
+import edu.princeton.cs.algs4.In;
 import edu.princeton.cs.algs4.StdDraw;
+import edu.princeton.cs.algs4.StdOut;
 
+import java.util.Arrays;
 import java.util.Comparator;
 import java.util.Objects;
 
@@ -78,7 +81,7 @@ public class Point implements Comparable<Point> {
             return Double.POSITIVE_INFINITY;
         else if (that.y == y)
             return 0.0;
-        return ((double) (that.y - y)) / ((double) (that.x - x));
+        return ((double) (that.y - y)) / (that.x - x);
     }
 
     /**
@@ -132,6 +135,29 @@ public class Point implements Comparable<Point> {
      * Unit tests the Point data type.
      */
     public static void main(String[] args) {
-        /* YOUR CODE HERE */
+        // read the n points from a file
+        In in = new In(Point.class.getResource("input48.txt"));
+        int n = in.readInt();
+        Point[] points = new Point[n];
+        for (int i = 0; i < n; i++) {
+            int x = in.readInt();
+            int y = in.readInt();
+            points[i] = new Point(x, y);
+        }
+
+        // draw the points
+        StdDraw.enableDoubleBuffering();
+        StdDraw.setXscale(0, 32768);
+        StdDraw.setYscale(0, 32768);
+        Arrays.stream(points).forEach(Point::draw);
+        StdDraw.show();
+
+        // print and draw the line segments
+        FastCollinearPoints collinear = new FastCollinearPoints(points);
+        for (LineSegment segment : collinear.segments()) {
+            StdOut.println(segment);
+            segment.draw();
+        }
+        StdDraw.show();
     }
 }
